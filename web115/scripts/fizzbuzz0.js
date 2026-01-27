059e598c880c3c71436a9c9dd253690fbdd70898
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-27 15:08:11 -0500

    Update fizzbuzz0.js

---
+++ web115/scripts/fizzbuzz0.js
@@ -0,0 +1,20 @@
+
+/* <script>
+        function updateGreeting() {
+            // Get values from input fields
+            let first = document.getElementById('first_name').value;
+            let middle = document.getElementById('middle_initial').value;
+            let last = document.getElementById('last_name').value;
+            
+            // Handle optional middle initial
+            let fullName = first + " ";
+            if (middle.length > 0) {
+                fullName += middle + ". ";
+            }
+            fullName += last;
+
+            // Update the H3 greeting
+            document.getElementById('greeting').innerText = "Welcome to John's Donut Shop, " + fullName + "!";
+        }
+    </script>
+    */
