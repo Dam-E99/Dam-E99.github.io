67b70d4f707e191f357f497f0997cdc01a757528
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-28 15:55:35 -0500

    added php copies

---
+++ web250/components/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <nav>
+            <a href="https://dam-e99.github.io/">Github Pages</a>
+            |
+            <a href="https://github.com/Dam-E99" target="_blank">Github Account</a>
+            |
+            <a href="https://www.freecodecamp.org/dame99" target="_blank">freeCodeCamp</a>
+            |
+            <a href="https://www.codecademy.com/profiles/dsotodu0" target="_blank">Codecademy</a>
+            |
+            <a href="https://www.linkedin.com/in/danny-soto-duarte-ab9744296" target="_blank">LinkedIn</a>
+        </nav>
+    </footer>
---
+++ web250/components/header.php
@@ -0,0 +1,10 @@
+<header>
+        <h1>WEB250</h1>
+        <nav>
+            <a href="/web250/index.html">Home</a>
+            |
+            <a href="introduction.html">Introduction</a>
+            |
+            <a href="contract.html">Contract</a>
+        </nav>
+    </header>
