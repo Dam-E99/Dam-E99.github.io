3c944e9bedf38056787342de17ce0c9b0dcc295f
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-28 15:36:54 -0500

    New PHP copies of HTML

---
+++ web250/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles/default.css" />
+    <title>Danny Soto Duarte's Dazzling Dodo 🐾 WEB250 Home </title>
+</head>
+<body>
+    <div data-include="components/header.html"></div>
+    <main>
+        <h2>Home</h2>
+        <p>This is the home page for my WEB250 course. Please feel free to look around. You can read more about me <a href="introduction.html">here.</a>
+           Thank you.</p>
+    </main>
+    <div data-include="components/footer.html"></div>
+<script src="scripts/HTMLInclude.js"></script>
+</body>
+</html>
---
+++ web250/introduction.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danny Soto Duarte's Dazzling Dodo 🐾 WEB250 Introduction</title>
+</head>
+<body>
+    <div data-include="components/header.html"></div>
+    <main>
+        <h2>Introduction</h2>
+        <p>This is the introduction page for my WEB250 course. Please feel free to look around. You can read more about me <a href="introduction.html">here.</a>
+           Thank you.</p>
+    </main>
+    <div data-include="components/footer.html"></div>
+    <script src="scripts/HTMLInclude.js"></script>
+</body>
+</html>
