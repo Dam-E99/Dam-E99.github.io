1f00f764671ca0d3a8bb2d3dd950017d5590b89b
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-29 16:19:34 -0500

    Update fizzbuzz0.js

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -2,11 +2,11 @@
 function updateGreeting() {
     // Grab values from the form
     const firstName = document.getElementById("first_name").value;
-    const middleName = document.getElementById("middle_initial").value;
+    const middleInitial = document.getElementById("middle_initial").value;
     const lastName = document.getElementById("last_name").value;
 
     // Construct the middle name part with period only if present
-    const formattedMiddle = middle_initial ? `${middle_initial}. ` : "";
+    const formattedMiddle = middleInitial ? `${middleInitial}. ` : "";
 
     // Construct final greeting and replace content
     const greetingText = `Welcome to Refresh Air HVAC ${firstName} ${formattedMiddle}${lastName}!`;
