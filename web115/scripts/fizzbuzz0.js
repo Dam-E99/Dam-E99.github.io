d056478668c559074910b5b0beb8d8a18867577b
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-30 11:28:04 -0500

    Update fizzbuzz0.js

--- web115/scripts/fizzbuzz0.js
+++ web115/scripts/fizzbuzz0.js
@@ -1,17 +1,18 @@
 
 function updateGreeting() {
-    // Grab values from the form
+    // Grab Values From the Form
     const firstName = document.getElementById("first_name").value;
     const middleInitial = document.getElementById("middle_initial").value;
     const lastName = document.getElementById("last_name").value;
 
-    // Construct the middle name part with period only if present
+    // Construct the Middle Name Part with Period Only if Present
     const formattedMiddle = middleInitial ? `${middleInitial}. ` : "";
 
-    // Construct final greeting and replace content
+    // Construct Final Greeting and Replace Content
     const greetingText = `Welcome to Refresh Air HVAC ${firstName} ${formattedMiddle}${lastName}!`;
     document.getElementById("greeting").textContent = greetingText;
 
+    //Prompt User for Number
     let limit = prompt(`How high do you want to count ${firstName}?`);
     limit = parseInt(limit);
 
