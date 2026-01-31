b75f321822cd17438295f4bb9934d3903580a622
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-01-31 15:54:08 -0500

    Update fizzbuzz1.js

--- web115/scripts/fizzbuzz1.js
+++ web115/scripts/fizzbuzz1.js
@@ -20,10 +20,12 @@ function updateGreeting() {
 
     for (let i = 1; i <= 140; i++ ) {
          let specialWord = "";
-        if (i % 3 === 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            specialWord = "Air Flow!";
+        } else if (i % 3 === 0) {
             specialWord = "Whoosh!";
         } else if (i % 5 === 0) {
-            specialWord = "Swish";
+            specialWord = "Swish!";
         } else {
             specialWord = "Cozy Breeze!";
         }
