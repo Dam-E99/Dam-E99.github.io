b18e60803e5738b4da43152c445524ca03603a49
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-02-13 14:45:16 -0500

    Update fizzbuzz4.js

--- web115/scripts/fizzbuzz4.js
+++ web115/scripts/fizzbuzz4.js
@@ -3,11 +3,12 @@ function checkDivision(number, divisor) {
 }
 
 function updateGreeting() {
-    // Grab Values From the Form
+    // Grab Name Values From the Form
     const firstName = document.getElementById("first_name").value;
     const middleInitial = document.getElementById("middle_initial").value;
     const lastName = document.getElementById("last_name").value;
 
+    // Grab Number/Word Values From the Form
     const limit = parseInt(document.getElementById("limit").value);
 
     const firstDivisor = parseInt(document.getElementById("divisor_one").value);
@@ -32,56 +33,35 @@ function updateGreeting() {
     const greetingText = `Welcome to Refresh Air HVAC ${firstName} ${formattedMiddle}${lastName}!`;
     document.getElementById("greeting").textContent = greetingText;
 
+
+    // Check For Number Validation 
+    if (isNaN(limit) || isNaN(firstDivisor) || isNaN(secondDivisor) || isNaN(thirdDivisor) ) {
+        alert("Invalid Input, Please Ensure All Fields Contain a Valid Number.");
+        return;
+    }
+
     // Number Output Loop 
     const listLoop = document.getElementById("loop");
     let htmlList = "";
-
-    //Prompt User for a Number
-    let userInput = prompt(`How high do you want to count ${firstName}?`);
-    // Check For Validation 
-    if (userInput.trim() === "" || isNaN(userInput)) {
-        alert("Invalid Input, Please Enter a Number.");
-        return;
-    }
-
-    // First Division Number Input
-    let firstDivInput = prompt("Enter your first divisor: ");
-    if (firstDivInput.trim() === "" || isNaN(firstDivInput)) {
-        alert("Invalid Input, Please Enter a Number.");
-        return;
-    }
-
-    // Second Division Number Input
-    let secondDivInput = prompt("Enter your second divisor: ");
-    if (secondDivInput.trim() === "" || isNaN(secondDivInput)) {
-        alert("Invalid Input, Please Enter a Number.");
-        return;
-    }
-
-    // Third Division Number Input
-    let thirdDivInput = prompt("Enter your third divisor: ");
-    if (thirdDivInput.trim() === "" || isNaN(thirdDivInput)) {
-        alert("Invalid Input, Please Enter a Number.");
-        return;
-    }
-
     
 
     // Updated logic to be more adaptable and modular
     for (let i = 1; i <= limit; i++ ) {
          let outputWord = "";
-        if (checkDivision(i % firstDivisor === 0)) {
+        if (checkDivision(i, firstDivisor)) {
             outputWord += word1;
         } 
-        if (checkDivision(i % secondDivisor === 0)) {
+        if (checkDivision(i, secondDivisor)) {
             outputWord += (outputWord ? ", " : "" ) + word2;
         } 
-        if (checkDivision(i % thirdDivisor === 0)){
+        if (checkDivision(i, thirdDivisor)){
             outputWord += (outputWord ? ", " : "" ) + word3;
         } 
+        //If No Match Use defaultWord(blank)
         if (outputWord === "") {
             outputWord = defaultWord;
         }
+        //If There is a Match use the Word
         if (outputWord != "") {
             outputWord += htmlList += `<li>${outputWord}</li>`;
         }
