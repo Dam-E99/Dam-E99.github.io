a4da1efd7477b668ba17033fbe12401dee057ce7
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-02-17 13:40:22 -0500

    Update header.php

--- web250/components/header.php
+++ web250/components/header.php
@@ -9,6 +9,6 @@
             |
             <a href="multipage_sites/superduper_static/index.htm">Multipage Static</a>
             |
-            <a href="multipage_sites/superduper_static/index.htm">Multipage PHP</a>
+            <a href="multipage_sites/superduper_static/index.php">Multipage PHP</a>
         </nav>
     </header>
