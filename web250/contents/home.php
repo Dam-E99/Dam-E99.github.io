6d591d0309f3b91a5ae3b4da21fd84db3aa62d8b
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-02-25 20:26:40 -0500

    Test

--- web250/contents/home.php
+++ web250/contents/home.php
@@ -1,3 +1,4 @@
 <h2>Home</h2>
+<h2>TESTING</h2>
         <p>This is the home page for my WEB250 course. Please feel free to look around. You can read more about me <a href="index.php?page=introduction">here.</a> Thank you.</p>
         <p>This course introduces dynamic (database-driven) website development. Topics include the use of basic database CRUD statements (create, read, update and delete) incorporated into web applications, as well as in software architecture principles.</p>
