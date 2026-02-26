d25abce927a9090278b156ed1d0b40a993a619a5
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-02-25 20:31:13 -0500

    Update contract.php

--- web250/contents/contract.php
+++ web250/contents/contract.php
@@ -1,5 +1,5 @@
 <h2>Contract</h2>
-        <p>I, <a href="introduction.html">Danny Soto Duarte</a>, agree to abide by the terms in my 2026 Spring WEB250-N850 Database Driven Websites with my instructor, Professor D.I. von Briesen.</p>
+        <p>I, <a href="index.php?page=introduction">Danny Soto Duarte</a>, agree to abide by the terms in my 2026 Spring WEB250-N850 Database Driven Websites with my instructor, Professor D.I. von Briesen.</p>
         <p>I understand that all work that I do on my school and personal websites will be publicly available to the world. I will not put information there that is inappropriate for schoolwork, or that I wish to keep private.</p>
         <p>I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.</p>
         <p>Signed: <em>Danny Soto Duarte, 23 February 2026</em></p>
