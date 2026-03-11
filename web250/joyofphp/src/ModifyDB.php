99d23688b33c8639cc84a193819f71696acba8ea
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-03-11 19:15:45 -0400

    Update ModifyDB.php

--- web250/joyofphp/src/ModifyDB.php
+++ web250/joyofphp/src/ModifyDB.php
@@ -3,24 +3,29 @@
  * Joy of PHP sample code
  * Demonstrates how to modify an existing database table.
  */
-
 include 'db.php';
 
-echo 'Connected successfully to MySQL.<br>';
+   if (!$mysqli) { 
+      die("Could not connect: ".$mysqli->error."<br>"); 
+  } 
+  echo 'Connected successfully to mySQL. <BR>'; 
+  
+//select a database to work with
+$mysqli->select_db("Cars");
+   Echo ("Selected the Cars database<br>");
 
-$query = "ALTER TABLE inventory ADD Primary_Image VARCHAR(250) NULL AFTER SALE_DATE";
-
+$query = "ALTER TABLE `inventory` ADD COLUMN IF NOT EXISTS `Primary_Image` VARCHAR(250) NULL AFTER `SALE_DATE`";
 echo "<p>***********</p>";
-echo $query;
+echo $query ;
 echo "<p>***********</p>";
-
-if ($mysqli->query($query) === TRUE) {
-    echo "Database table 'INVENTORY' modified";
-} else {
-    echo "Error: " . $mysqli->error . "<br>";
+if ($mysqli->query($query) === TRUE) 
+{
+    echo "Database table 'INVENTORY' modified</P>";
 }
-
+else
+{
+    echo "<p>Error: </p>" . $mysqli->error."<br>";;
+}
 $mysqli->close();
-
 echo "<br><br><a href='index.html'>Home</a>";
 ?>
