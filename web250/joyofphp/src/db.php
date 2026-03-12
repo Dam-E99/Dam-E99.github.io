83a704d0f46aa8ab43f7c8c803819673c7b6172e
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-03-12 15:21:13 -0400

    Update db.php

--- web250/joyofphp/src/db.php
+++ web250/joyofphp/src/db.php
@@ -1,9 +1,30 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "root", "Cars");
+/* Detect environment */
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
 
+    // LOCAL (MAMP)
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "Cars";
+
+} else {
+
+    // HOSTED SERVER
+    $host = "your_host";
+    $username = "your_db_username";
+    $password = "your_db_password";
+    $database = "your_db_name";
+
+}
+
+/* Create connection */
+$mysqli = new mysqli($host, $username, $password, $database);
+
+/* Check connection */
 if ($mysqli->connect_errno) {
-    die("Connection failed: " . $mysqli->connect_error);
+    die("Database connection failed: " . $mysqli->connect_error);
 }
 
 ?>
