1a9d87c2d1d5d22d99b8333adf6d08b05323d8de
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-03-12 14:20:12 -0400

    Update ModifyDB.php

--- web250/joyofphp/src/ModifyDB.php
+++ web250/joyofphp/src/ModifyDB.php
@@ -23,7 +23,7 @@ if ($result->num_rows == 0) {
     $mysqli->query($query);
     echo "Column added.";
 } else {
-    echo "Column already exists, skipping...";
+    echo "Column already exists";
 }
 
 ?>
