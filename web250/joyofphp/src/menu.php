bd5b647ecbe4801c53eab019100abfaaaeea17c8
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-03-12 14:32:41 -0400

    Update menu.php

--- web250/joyofphp/src/menu.php
+++ web250/joyofphp/src/menu.php
@@ -1,6 +1,10 @@
 <nav>
     <a href="samsusedcars.html">Home</a>
+    |
     <a href="ViewCars.php">View Cars</a>
+    |
     <a href="formEnterCar.htm">Add Car</a>
+    |
+    <a href="EditCar.php">Edit Cars</a>
 </nav>
 <hr>
