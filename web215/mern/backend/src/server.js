38ecb8d4b430b38ba1e1cb5cea6b56d0a9e63d1e
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-04-05 20:42:56 -0400

    MERN TUTORIAL

--- web215/mern/backend/src/server.js
+++ web215/mern/backend/src/server.js
@@ -1,13 +1,39 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.use("/api/notes", notesRoutes)
+
+// middleware
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+app.use(express.json()); // Parse HSON bodies: req.body
+app.use(rateLimiter);
 
 
-app.listen(5001, () => {
+//Simple Custom middleware
+// app.use((req, res, next) => {
+//    console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//    next();
+// });
 
-    console.log("Server started on PORT: 5001");
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT: ", PORT);
+    });
 });
 
