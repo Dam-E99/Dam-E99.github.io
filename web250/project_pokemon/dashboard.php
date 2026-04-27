24a5ecbec614fde1179581b74c226a494961cd18
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-04-27 18:50:16 -0400

    pokelot

--- web250/project_pokemon/dashboard.php
+++ web250/project_pokemon/dashboard.php
@@ -5,5 +5,9 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
-
 ?>
+
+<h2>Welcome to PokeLot</h2>
+
+<a href="add_card.php">Add Card</a> |
+<a href="logout.php">Logout</a>
