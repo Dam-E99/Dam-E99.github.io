79bd407daf1046b3c695afe097fc8706f0c5ed83
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-09-03 13:04:48 -0400

    Full javascript integration

--- web140/week1/scripts/script.js
+++ web140/week1/scripts/script.js
@@ -1,19 +1,30 @@
 
 let score = 0;
 
-function checkAnswer(isCorrect) {
-    let message = document.getElementById("score");
-    if (isCorrect === True) {
+function checkAnswer(isCorrect, messageId) {
+    let message = document.getElementById(messageId);
+
+    if (isCorrect === true) {
         message.textContent = "Correct!";
-        score += 1;
+        message.style.color = "#007FD7"
     } else {
         message.textContent = "Incorrect. Try again!";
+        message.style.color = "#EA1818"
     }
+
 }
 
 let submitBtn = document.getElementById("check_quiz");
 
-let submitBtn = addEventListener("click", function() {
+submitBtn.addEventListener("click", function() {
     let scoreDisplay = document.getElementById("score");
-    scoreDisplay.textContent = "Your Score: " + score + " out of 6";
+    let correctBtn = document.getElementsByClassName("correctAns");
+    let finalScore = 0
+    for (let i = 0; i < correctBtn.length; i++) {
+        if (correctBtn[i].checked === true) {
+            finalScore += 1;
+        }
+    }
+
+    scoreDisplay.textContent = "Your Score: " + finalScore + " out of 6";
 });
