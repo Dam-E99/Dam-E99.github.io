e55a62399f2e5487013f1a6d53b7375e86c3cc0b
Danny <151582153+Dam-E99@users.noreply.github.com>
2026-09-11 14:23:38 -0400

    script.js JS from CodePen

---
+++ web140/script.js
@@ -0,0 +1,5 @@
+const welcomeButton = document.querySelector("#welcomeButton");
+
+welcomeButton.addEventListener("click", function () {
+    welcomeButton.textContent = "Thanks for visiting!";
+});
